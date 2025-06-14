6e413de6dec7ad7fd272acafb6d3392a2fc85be8
Alexandre <alevaristofig@gmail.com>
2025-06-14 17:25:33 -0300

    Criação da controller lista tarefa

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ListaController;
 use App\Http\Controllers\Api\TarefaController;
+use App\Http\Controllers\Api\ListaTarefaController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,14 +15,21 @@ Route::prefix('v1/todo')->group(function() {
     Route::group([
         'as' => 'listas'
         //'middleware'=> \Tymon\JWTAuth\Http\Middleware\Authenticate::class
-    ], function() {       
+        ], function() {       
         Route::resource('listas',ListaController::class);
     });
 
     Route::group([
         'as' => 'tarefas'
         //'middleware'=> \Tymon\JWTAuth\Http\Middleware\Authenticate::class
-    ], function() {       
+        ], function() {       
         Route::resource('tarefas',TarefaController::class);
+    });
+
+     Route::group([
+        'as' => 'listatarefas'
+        //'middleware'=> \Tymon\JWTAuth\Http\Middleware\Authenticate::class
+        ], function() {       
+        Route::resource('listatarefas',ListaTarefaController::class);
     });
 });
