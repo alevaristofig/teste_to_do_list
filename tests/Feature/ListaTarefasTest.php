137f88fa356f5f9f8c2802f874e9947d8cada31c
Alexandre <alevaristofig@gmail.com>
2025-06-14 13:00:57 -0300

    Implementação dos testes de buscar, atualizar e remover lista

--- tests/Feature/ListaTarefasTest.php
+++ tests/Feature/ListaTarefasTest.php
@@ -10,8 +10,7 @@ use App\Models\ListaTarefa;
 
 class ListaTarefasTest extends TestCase
 {
-    public function test_inserirListaDeTarefasSucesso(): void
-    {
+    public function test_inserirListaDeTarefasSucesso(): void {
         $dados = [
             'id'=> 1,
             'titulo'=> 'Lista Tarefa Exemplo'
@@ -28,6 +27,68 @@ class ListaTarefasTest extends TestCase
         $this->assertEquals('Lista Tarefa Exemplo Exemplo',$result->titulo);
         $this->assertEquals(1,$result->id);
 
-        Mockery::close();
+       // Mockery::close();
+    }
+
+    public function test_buscarListaDeTarefasSucesso(): void {
+
+        $id = 1;
+        $lista = new ListaTarefa();
+
+        $lista->id = 1;
+        $lista->titulo = "Buscar Lista Tarefa Exemplo";
+
+        $mock = Mockery::mock('alias:' . ListaTarefa::class);
+        $mock->shouldReceive('find')
+            ->once()    
+            ->with($id)        
+            ->andReturn($lista);
+
+        $result = ListaTarefa::find($id);
+
+        $this->assertEquals(1,$result->id);
+        $this->assertEquals('Buscar Lista Tarefa Exemplo',$result->titulo);
+    }
+
+    public function test_atualizarListaDeTarefasSucesso(): void {
+
+        $id = 1;
+        $lista = new ListaTarefa();
+        $lista->titulo = "Atualizar Lista Tarefa Exemplo";
+
+        $mock = Mockery::mock('alias:' . ListaTarefa::class);
+        $mock->shouldReceive('find')
+            ->once()    
+            ->with($id)        
+            ->andReturn($lista);
+
+        $listaUpdate = ListaTarefa::find($id);
+        
+        $listaUpdate->titulo = "Atualizar Lista Tarefa Exemplo 2";
+
+        $mock->shouldReceive('create')
+            ->once()
+            ->with($listaUpdate)
+            ->andReturn($listaUpdate);
+
+        $result = ListaTarefa::create($listaUpdate);
+
+        $this->assertInstanceOf(ListaTarefa::class,$result);
+        $this->assertEquals("Atualizar Lista Tarefa Exemplo 2",$result->titulo);        
+    }
+
+    public function test_deletarListaDeTarefasSucesso(): void {
+
+        $id = 1;
+        $mock = Mockery::mock('alias:' . ListaTarefa::class);     
+
+        $mock->shouldReceive('delete')
+            ->once()
+            ->with($id)
+            ->andReturnTrue();
+
+        $result = ListaTarefa::delete($id);
+       
+        $this->assertTrue($result);                
     }
 }
