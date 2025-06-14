1e7b145c2a9dd09187e691c7852f3e82229269ee
Alexandre <alevaristofig@gmail.com>
2025-06-14 10:57:42 -0300

    Criação do model ListaTarefa

---
+++ app/Models/ListaTarefa.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ListaTarefa extends Model
+{
+    protected $fillable = [
+        'titulo'
+    ];
+}
--- tests/Feature/ListaTarefasTest.php
+++ tests/Feature/ListaTarefasTest.php
@@ -6,14 +6,28 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Mockery;
+use App\Models\ListaTarefa;
 
 class ListaTarefasTest extends TestCase
 {
-    public function test_example(): void
+    public function test_inserirListaDeTarefasSucesso(): void
     {
         $dados = [
             'id'=> 1,
             'titulo'=> 'Lista Tarefa Exemplo'
         ];
+
+        $mock = Mockery::mock('alias' . ListaTarefa::class);
+        $mock->shouldReceive('create')
+             ->once()
+             ->with($dados)
+             ->andReturn((object) $dados);
+
+        $result = ListaTarefa::create($dados);
+
+        $this->assertEquals('Lista Tarefa Exemplo Exemplo',$result->titulo);
+        $this->assertEquals(1,$result->id);
+
+        Mockery::close();
     }
 }
