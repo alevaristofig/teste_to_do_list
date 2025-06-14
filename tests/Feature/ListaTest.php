bc5e3568cb148f61804b080bd1f3f1c940e9ec79
Alexandre <alevaristofig@gmail.com>
2025-06-14 16:58:33 -0300

    Refatoração dos testes de listas

--- tests/Feature/ListaTest.php
+++ tests/Feature/ListaTest.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Mockery;
-use App\Models\ListaTarefa;
+use App\Models\Lista;
 
-class ListaTarefasTest extends TestCase
+class ListaTest extends TestCase
 {
     public function test_inserirListaDeTarefasSucesso(): void {
         $dados = [
@@ -16,35 +16,35 @@ class ListaTarefasTest extends TestCase
             'titulo'=> 'Lista Tarefa Exemplo'
         ];
 
-        $mock = Mockery::mock('alias' . ListaTarefa::class);
+        $mock = Mockery::mock('alias' . Lista::class);
         $mock->shouldReceive('create')
              ->once()
              ->with($dados)
              ->andReturn((object) $dados);
 
-        $result = ListaTarefa::create($dados);
+        $result = Lista::create($dados);
 
         $this->assertEquals('Lista Tarefa Exemplo Exemplo',$result->titulo);
         $this->assertEquals(1,$result->id);
 
-       // Mockery::close();
+        Mockery::close();
     }
 
     public function test_buscarListaDeTarefasSucesso(): void {
 
         $id = 1;
-        $lista = new ListaTarefa();
+        $lista = new \stdClass();
 
         $lista->id = 1;
         $lista->titulo = "Buscar Lista Tarefa Exemplo";
 
-        $mock = Mockery::mock('alias:' . ListaTarefa::class);
+        $mock = Mockery::mock('alias:' . Lista::class);
         $mock->shouldReceive('find')
             ->once()    
             ->with($id)        
             ->andReturn($lista);
 
-        $result = ListaTarefa::find($id);
+        $result = Lista::find($id);
 
         $this->assertEquals(1,$result->id);
         $this->assertEquals('Buscar Lista Tarefa Exemplo',$result->titulo);
@@ -53,16 +53,16 @@ class ListaTarefasTest extends TestCase
     public function test_atualizarListaDeTarefasSucesso(): void {
 
         $id = 1;
-        $lista = new ListaTarefa();
+        $lista = new Lista();//new \stdClass();
         $lista->titulo = "Atualizar Lista Tarefa Exemplo";
 
-        $mock = Mockery::mock('alias:' . ListaTarefa::class);
+        $mock = Mockery::mock('alias:' . Lista::class);
         $mock->shouldReceive('find')
             ->once()    
             ->with($id)        
             ->andReturn($lista);
 
-        $listaUpdate = ListaTarefa::find($id);
+        $listaUpdate = Lista::find($id);
         
         $listaUpdate->titulo = "Atualizar Lista Tarefa Exemplo 2";
 
@@ -71,23 +71,23 @@ class ListaTarefasTest extends TestCase
             ->with($listaUpdate)
             ->andReturn($listaUpdate);
 
-        $result = ListaTarefa::create($listaUpdate);
+        $result = Lista::create($listaUpdate);
 
-        $this->assertInstanceOf(ListaTarefa::class,$result);
+        $this->assertInstanceOf(Lista::class,$result);
         $this->assertEquals("Atualizar Lista Tarefa Exemplo 2",$result->titulo);        
     }
 
     public function test_deletarListaDeTarefasSucesso(): void {
 
         $id = 1;
-        $mock = Mockery::mock('alias:' . ListaTarefa::class);     
+        $mock = Mockery::mock('alias:' . Lista::class);     
 
         $mock->shouldReceive('delete')
             ->once()
             ->with($id)
             ->andReturnTrue();
 
-        $result = ListaTarefa::delete($id);
+        $result = Lista::delete($id);
        
         $this->assertTrue($result);                
     }
