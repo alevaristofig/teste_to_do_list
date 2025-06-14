a4787d40a93d482d9c1053386aeb0ef5d55dc801
Alexandre <alevaristofig@gmail.com>
2025-06-14 13:12:42 -0300

    Implementação dos testes de atualizar de tarefas

--- tests/Feature/TarefasTest.php
+++ tests/Feature/TarefasTest.php
@@ -54,4 +54,35 @@ class TarefasTest extends TestCase
         $this->assertEquals(1,$result->id);
         $this->assertEquals('Buscar Tarefa Exemplo',$result->titulo);
     }
+
+    public function test_atualizarTarefasSucesso(): void {
+
+        $id = 1;
+        $lista = new Tarefa();//new \stdClass();
+        $lista->titulo = "Atualizar Tarefa Exemplo";
+        $lista->tempo = "1 dia";
+        $lista->dificuldade = 5;
+
+        $mock = Mockery::mock('alias:' . Tarefa::class);
+        $mock->shouldReceive('find')
+            ->once()    
+            ->with($id)        
+            ->andReturn($lista);
+
+        $listaUpdate = Tarefa::find($id);
+        
+        $listaUpdate->titulo = "Atualizar Tarefa Exemplo 2";
+        $listaUpdate->tempo = "3 dia";
+        $listaUpdate->dificuldade = 3;
+
+        $mock->shouldReceive('create')
+            ->once()
+            ->with($listaUpdate)
+            ->andReturn($listaUpdate);
+
+        $result = Tarefa::create($listaUpdate);
+
+        $this->assertInstanceOf(Tarefa::class,$result);
+        $this->assertEquals("3 dia",$result->tempo);        
+    }
 }
